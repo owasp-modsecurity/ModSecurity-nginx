c523f2f236ca260d3dbfea0f987ae7397681866e
Alexey Zelkin <phantom@freebsd10-amd64-phantom.gnt.nginx.com>
2016-07-04 13:52:14 +0000

    Include method 'HEAD' to list of allowed methods

--- src/ngx_http_modsecurity_log.c
+++ src/ngx_http_modsecurity_log.c
@@ -47,9 +47,10 @@ ngx_http_modsecurity_log_handler(ngx_http_request_t *r)
         return NGX_OK;
     }
 
-    if (r->method != NGX_HTTP_GET && r->method != NGX_HTTP_POST) {
+    if (r->method != NGX_HTTP_GET &&
+        r->method != NGX_HTTP_POST && r->method != NGX_HTTP_HEAD) {
         dd("ModSecurity is not ready to deal with anything different from " \
-            "POST or GET");
+            "POST, GET or HEAD");
         return NGX_OK;
     }
 

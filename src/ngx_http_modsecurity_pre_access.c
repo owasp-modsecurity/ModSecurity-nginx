4f153f4c516bacfbe05f9e7de4b6a9fb79560f99
Felipe Zimmerle <felipe@zimmerle.org>
2016-09-19 21:15:24 -0300

    Temporary inspect all kind of requests

--- src/ngx_http_modsecurity_pre_access.c
+++ src/ngx_http_modsecurity_pre_access.c
@@ -54,12 +54,17 @@ ngx_http_modsecurity_pre_access_handler(ngx_http_request_t *r)
         dd("ModSecurity not enabled... returning");
         return NGX_DECLINED;
     }
+    /*
+     * FIXME:
+     * In order to perform some tests, let's accept everything.
+     *
     if (r->method != NGX_HTTP_GET &&
         r->method != NGX_HTTP_POST && r->method != NGX_HTTP_HEAD) {
         dd("ModSecurity is not ready to deal with anything different from " \
             "POST, GET or HEAD");
         return NGX_DECLINED;
     }
+    */
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_modsecurity_module);
 

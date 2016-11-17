f39e2a2b1241053df07b0323e1ee1077002459d4
Andrei Belov <defanator@gmail.com>
2016-11-17 22:58:25 +0300

    Removed str_replace() function introduced in 2de9b2a.
    Using the "%s" format for ngx_log_error() seems to be enough.

--- src/ngx_http_modsecurity_log.c
+++ src/ngx_http_modsecurity_log.c
@@ -21,50 +21,14 @@
 #include "ngx_http_modsecurity_common.h"
 
 
-// FIXME: This function should be replaced, it is being used just for test.
-char *
-str_replace ( const char *string, const char *substr, const char *replacement ){
-  char *tok = NULL;
-  char *newstr = NULL;
-  char *oldstr = NULL;
-  char *head = NULL;
-
-  /* if either substr or replacement is NULL, duplicate string a let caller handle it */
-  if ( substr == NULL || replacement == NULL ) return strdup (string);
-  newstr = strdup (string);
-  head = newstr;
-  while ( (tok = strstr ( head, substr ))){
-    oldstr = newstr;
-    newstr = malloc ( strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) + 1 );
-    /*failed to alloc mem, free old string and return NULL */
-    if ( newstr == NULL ){
-      free (oldstr);
-      return NULL;
-    }
-    memcpy ( newstr, oldstr, tok - oldstr );
-    memcpy ( newstr + (tok - oldstr), replacement, strlen ( replacement ) );
-    memcpy ( newstr + (tok - oldstr) + strlen( replacement ), tok + strlen ( substr ), strlen ( oldstr ) - strlen ( substr ) - ( tok - oldstr ) );
-    memset ( newstr + strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) , 0, 1 );
-    /* move back head right after the last replacement */
-    head = newstr + (tok - oldstr) + strlen( replacement );
-    free (oldstr);
-  }
-  return newstr;
-}
-
-
 void
 ngx_http_modsecurity_log(void *log, const char* msg)
 {
-	char *new_log = NULL;
-
     if (log == NULL) {
         return;
     }
 
-    new_log = str_replace(msg, "%", "_");
-    ngx_log_error(NGX_LOG_INFO, (ngx_log_t *)log, 0, new_log);
-    free(new_log);
+    ngx_log_error(NGX_LOG_INFO, (ngx_log_t *)log, 0, "%s", msg);
 }
 
 
